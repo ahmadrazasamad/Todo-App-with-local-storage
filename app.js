815e16bef911ce3d4d29cf88fc18c9315a069422
Ahmad Raza <ahmadrazasamad@gmail.com>
2023-08-30 09:47:04 +0500

    Additional input fields added in todo
    New additional fields added in the todo for example description, list, due date, and due time

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-let newTodo = document.getElementById("new-todo");
-let todosList = document.getElementById("todos");
+const allTodosDiv = document.getElementById("all-todos"); //div
+const todosList = document.getElementById("todos"); //ul
+const addNewTodoDiv = document.getElementById("add-todo"); //div
 
-const addTodo = () => {
+let addNewTodo = () => {
+    allTodosDiv.setAttribute("style", "display:none !important")
+    addNewTodoDiv.removeAttribute("style")
+}
+
+let addTodo = () => {
+    const newTodoInput = document.getElementById("new-todo"); //input
+    const newTodoDescription = document.getElementById("todo-description");
     let todos = localStorage.getItem("todos");
     if (!todos) {
         todos = [];
@@ -9,22 +17,26 @@ const addTodo = () => {
     else {
         todos = JSON.parse(todos);
     }
-    todos.push(newTodo.value);
+    const newTodo = {
+        title: newTodoInput.value,
+        description: newTodoDescription.value
+    }
+    todos.push(newTodo);
     localStorage.setItem("todos", JSON.stringify(todos));
-    todosList.innerHTML += `<li>${newTodo.value}<button onclick="editTodo()">Edit</button><button onclick="deleteTodo()">Delete</button></li>`;
-    newTodo.value = "";
+    todosList.innerHTML += `<li>${newTodoInput.value}<button onclick="editTodo()">Edit</button><button onclick="deleteTodo()">Delete</button></li>`;
+    newTodoInput.value = "";
 }
 
-const getTodos = () => {
+let getTodos = () => {
     let oldTodos = JSON.parse(localStorage.getItem("todos"));
     for (let i = 0; i < oldTodos.length; i++) {
         todosList.innerHTML += `<li>${oldTodos[i]}<button onclick="editTodo()">Edit</button><button onclick="deleteTodo()">Delete</button></li>`;
     }
 }
 
-const editTodo = () => {
+let editTodo = () => {
     console.log("hello");
 }
-const deleteTodo = () => {
+let deleteTodo = () => {
     console.log("world");
 }
