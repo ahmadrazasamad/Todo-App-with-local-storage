9528eb33a0378d2681e46239b7aeca1d3d8d0032
Ahmad Raza <ahmadrazasamad@gmail.com>
2023-09-03 16:43:54 +0500

    Add todo's functionality is completely done
    Add todo functionality done, further will work on getting todo's functionality

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
+let getTodos = () => {
+    let oldTodos = JSON.parse(localStorage.getItem("todos"));
+    if (oldTodos === null) {
+        const addFirstTodoImg = document.getElementById("add-first-todo");
+        addFirstTodoImg.removeAttribute("style");
+    }
+    else {
+        if (oldTodos.length > 0) {
+            for (let i = 0; i < oldTodos.length; i++) {
+                todosList.innerHTML += "Hello World";
+            }
+        }
+        else {
+            const noTodo = document.getElementById("no-todo");
+            noTodo.removeAttribute("style");
+        }
+    }
+}
+
 const allTodosDiv = document.getElementById("all-todos"); //div
 const todosList = document.getElementById("todos"); //ul
 const addNewTodoDiv = document.getElementById("add-todo"); //div
 
 let addNewTodo = () => {
-    allTodosDiv.setAttribute("style", "display:none !important")
-    addNewTodoDiv.removeAttribute("style")
+    allTodosDiv.setAttribute("style", "display:none !important");
+    addNewTodoDiv.removeAttribute("style");
 }
 
+const newTodoTitle = document.getElementById("new-todo"); //input
+const newTodoDescription = document.getElementById("todo-description");
+const newTodoList = document.getElementById("task-list");
+const dueDateInput = document.getElementById("due-date");
+const dueTimeInput = document.getElementById("due-time");
+dueDateInput.addEventListener("change", () => {
+    if (dueDateInput.value) {
+        dueTimeInput.removeAttribute("style");
+    }
+});
+
 let addTodo = () => {
-    const newTodoInput = document.getElementById("new-todo"); //input
-    const newTodoDescription = document.getElementById("todo-description");
+    let dueDate = new Date(`${dueDateInput.value} ${dueTimeInput.value}`);
+    let dueDateTxt;
+    if (!dueDate) {
+        dueDateTxt = `${dueDate.toDateString()} ${(dueDate.toLocaleTimeString()).slice(0, 5)} ${(dueDate.toLocaleTimeString()).slice(8)}`;
+    }
     let todos = localStorage.getItem("todos");
     if (!todos) {
         todos = [];
@@ -17,20 +50,39 @@ let addTodo = () => {
     else {
         todos = JSON.parse(todos);
     }
-    const newTodo = {
-        title: newTodoInput.value,
-        description: newTodoDescription.value
-    }
-    todos.push(newTodo);
-    localStorage.setItem("todos", JSON.stringify(todos));
-    todosList.innerHTML += `<li>${newTodoInput.value}<button onclick="editTodo()">Edit</button><button onclick="deleteTodo()">Delete</button></li>`;
-    newTodoInput.value = "";
-}
-
-let getTodos = () => {
-    let oldTodos = JSON.parse(localStorage.getItem("todos"));
-    for (let i = 0; i < oldTodos.length; i++) {
-        todosList.innerHTML += `<li>${oldTodos[i]}<button onclick="editTodo()">Edit</button><button onclick="deleteTodo()">Delete</button></li>`;
+    if ((newTodoTitle.value).trim() !== "") {
+        const newTodo = {
+            title: newTodoTitle.value,
+            description: newTodoDescription.value,
+            dueDate,
+            taskList: newTodoList.value
+        }
+        let newTodoItem = `<div class="todo-item d-flex">
+        <input type="checkbox" />
+        <div>
+            <h6 class="no-margin">${newTodoTitle.value}</h6>`;
+        if (dueDateTxt) {
+            newTodoItem += `<p class="no-margin">${dueDateTxt}</p>`;
+        }
+        if (newTodoList.value !== "default") {
+            newTodoItem += `<p class="no-margin">${newTodoList.value}</p>`;
+        }
+        newTodoItem += `</div>
+        </div>`;
+        todosList.innerHTML += newTodoItem;
+        todos.push(newTodo);
+        addNewTodoDiv.setAttribute("style", "display:none !important");
+        allTodosDiv.removeAttribute("style");
+        localStorage.setItem("todos", JSON.stringify(todos));
+    } else {
+        newTodoTitle.className += " red-border";
+        newTodoTitle.nextSibling.nextSibling.removeAttribute("style");
+        newTodoTitle.addEventListener("input", (e) => {
+            if ((e.target.value).length > 0) {
+                newTodoTitle.classList.remove("red-border");
+                newTodoTitle.nextSibling.nextSibling.setAttribute("style", "display: none !important");
+            }
+        })
     }
 }
 
